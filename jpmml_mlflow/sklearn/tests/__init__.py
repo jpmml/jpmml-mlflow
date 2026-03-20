051783ff559383792a03db8ed4c32191486a925d
Villu Ruusmann <villu.ruusmann@gmail.com>
2026-03-20 22:54:59 +0200

    Updated test resources

--- jpmml_mlflow/sklearn/tests/__init__.py
+++ jpmml_mlflow/sklearn/tests/__init__.py
@@ -1,10 +1,14 @@
+from jpmml_mlflow.pmml.tests import _find_resource
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 
+import pandas
+
 def _make_sk_model():
-	iris = load_iris(as_frame = True)
-	iris_X = iris.data
-	iris_y = iris.target
+	df = pandas.read_csv(_find_resource("Iris.csv"))
+
+	iris_X = df[df.columns[0:4]]
+	iris_y = df["Species"]
 
 	model = DecisionTreeClassifier(max_depth = 3, random_state = 42)
 	model.fit(iris_X, iris_y)
