e7f9f364fc3866a769b3f6b5ada5b6a9ebf6cb9e
Villu Ruusmann <villu.ruusmann@gmail.com>
2026-03-21 10:51:30 +0200

    Ensured compatibility with MLflow 2.0.1

--- jpmml_mlflow/sklearn/tests/test_sklearn.py
+++ jpmml_mlflow/sklearn/tests/test_sklearn.py
@@ -1,6 +1,7 @@
 from jpmml_mlflow.tests import MLflowTest
 from jpmml_mlflow.sklearn import log_model
 from jpmml_mlflow.sklearn.tests import _make_sk_model
+from mlflow.artifacts import download_artifacts
 from mlflow.models import Model
 
 import mlflow
@@ -11,6 +12,7 @@ class SkLearnTest(MLflowTest):
 		sk_model = _make_sk_model()
 		with mlflow.start_run() as run:
 			log_model(sk_model, artifact_path = "model")
-		mlflow_sk_model = Model.load(f"runs:/{run.info.run_id}/model")
+		model_path = download_artifacts(f"runs:/{run.info.run_id}/model")
+		mlflow_sk_model = Model.load(model_path)
 		self.assertIn("sklearn", mlflow_sk_model.flavors)
 		self.assertIn("pmml", mlflow_sk_model.flavors)
