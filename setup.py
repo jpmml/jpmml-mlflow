674ad1d2ff9f564c9eaeb42ed5eda1a9879a7d8e
Villu Ruusmann <villu.ruusmann@gmail.com>
2026-03-26 12:41:44 +0200

    Registered 'jpmml-mlflow' package with PyPI

--- setup.py
+++ setup.py
@@ -5,12 +5,27 @@ exec(open("jpmml_mlflow/metadata.py").read())
 setup(
 	name = "jpmml-mlflow",
 	version = __version__,
+	description = "PMML model flavors for MLflow",
 	author = "Villu Ruusmann",
 	author_email = "villu.ruusmann@gmail.com",
+	url = "https://github.com/jpmml/jpmml-mlflow",
+	download_url = "https://github.com/jpmml/jpmml-mlflow/archive/" + __version__ + ".tar.gz",
 	license = __license__,
+	classifiers = [
+		"Development Status :: 5 - Production/Stable",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Science/Research",
+		"Topic :: Software Development",
+		"Topic :: Scientific/Engineering"
+	],
 	packages = find_packages(exclude = ["*.tests.*", "*.tests"]),
 	package_data = {
 		"" : ["resources/*.jar"],
+	},
+	exclude_package_data = {
+		"" : ["README.md"],
 	},
 	python_requires = ">=3.8",
 	install_requires = [
