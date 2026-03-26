6b5251a7aa5fe59affe4a135073119ebb776e88d
Villu Ruusmann <villu.ruusmann@gmail.com>
2026-03-26 12:02:00 +0200

    Refactored unit tests

--- jpmml_mlflow/evaluator_spark/tests/test_evaluator_spark.py
+++ jpmml_mlflow/evaluator_spark/tests/test_evaluator_spark.py
@@ -1,4 +1,5 @@
-from jpmml_mlflow.tests import _load_resource, PySparkTest
+from jpmml_evaluator_pyspark import FlatPMMLTransformer, NestedPMMLTransformer
+from jpmml_mlflow.tests import _find_resource, _load_resource, PySparkTest
 from jpmml_mlflow.evaluator_spark import classpath, load_model, log_model
 from py4j.java_gateway import JavaObject
 from typing import List
@@ -30,8 +31,17 @@ class JPMMLEvaluatorSparkTest(PySparkTest):
 		self.assertIsNotNone(evaluator)
 		self.assertIsInstance(evaluator, JavaObject)
 
-		flatPmmlTransformer = jvm.org.jpmml.evaluator.spark.FlatPMMLTransformer(evaluator)
-		self.assertEqual(1 + 3, (flatPmmlTransformer.pmmlFields()).size())
+		df = self._spark.read.csv(_find_resource("Iris.csv"), header = True, inferSchema = True)
+		df = df.drop("Species")
 
-		nestedPmmlTransformer = jvm.org.jpmml.evaluator.spark.NestedPMMLTransformer(evaluator)
-		self.assertEqual(1 + 3, (nestedPmmlTransformer.pmmlFields()).size())
+		flatPmmlTransformer = FlatPMMLTransformer(evaluator)
+
+		pmml_df = flatPmmlTransformer.transform(df)
+		self.assertEqual(150, pmml_df.count())
+		self.assertEqual(4 + (1 + 3) + 1, len(pmml_df.columns))
+
+		nestedPmmlTransformer = NestedPMMLTransformer(evaluator)
+
+		pmml_df = nestedPmmlTransformer.transform(df)
+		self.assertEqual(150, pmml_df.count())
+		self.assertEqual(4 + (1) + 1, len(pmml_df.columns))
